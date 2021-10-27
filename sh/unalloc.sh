aa5f4b7a1b59d2c5c218b9dfa18790961354e35c
Boris Burkov <boris@bur.io>
2021-10-27 11:40:57 -0700

    make desired more modular

--- sh/unalloc.sh
+++ sh/unalloc.sh
@@ -2,8 +2,8 @@
 
 GB=$((1024 * 1024 * 1024))
 
-# dusage from 30-60
-MAX_TRIES=4
+# dusage from 30-70
+MAX_TRIES=5
 DUSAGE_START=30
 DUSAGE_STEP=10
 BALANCE_LIMIT=5
@@ -28,8 +28,6 @@ _uuid() {
   btrfs fi show $mnt | grep uuid: | awk '{print $4}'
 }
 
-_meta
-
 _force_alloc() {
   local uuid=$1
   echo 1 > /sys/fs/btrfs/$uuid/allocation/metadata/force_chunk_alloc
@@ -37,7 +35,8 @@ _force_alloc() {
 
 _try_balance() {
   local mnt=$1
-  local desired_bytes=$2
+  local desired=$2
+  local desired_bytes=$(($desired * $GB))
   local tries=0
   local dusage=$DUSAGE_START
   local add_to_tier=0
@@ -50,7 +49,7 @@ _try_balance() {
 
   while [ $unalloc -lt $want ] && [ $tries -lt $MAX_TRIES ]; do
     echo "unalloc $unalloc less than $want. Balance with dusage $dusage."
-    btrfs filesystem balance start -dusage=$dusage,limit=$BALANCE_LIMIT $mnt
+    btrfs filesystem balance start -dusage=$dusage,limit=$desired $mnt
     let dusage+=10
     let tries+=1
     unalloc=$(_unalloc $mnt)
@@ -73,7 +72,7 @@ _balance_and_alloc() {
   local uuid=$(_uuid $mnt)
   local meta=$(_meta $mnt)
 
-  _try_balance $mnt $desired_bytes
+  _try_balance $mnt $desired
 
   while [ $meta -lt $desired_bytes ] && [ $tries -lt $desired ]; do
     _force_alloc $uuid
@@ -85,5 +84,5 @@ _balance_and_alloc() {
 }
 
 _balance_and_alloc / 5
-#_try_balance /data $((10 * GB))
-#_try_balance /data/device00/mount_point $((10 * GB))
+#_balance_and_alloc /data 10
+#_balance_and_alloc /data/device00/mount_point 10
