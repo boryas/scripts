1eb1844e407f793c650e8bd2c765975001b0935d
Boris Burkov <boris@bur.io>
2021-11-04 09:59:22 -0700

    sign extension example script in c

---
+++ c/sign-ext.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define SZ_2G (1 << 31)
+#define NEG_1 (-1)
+
+int main() {
+	unsigned long long ull_2g = SZ_2G;
+	unsigned long long ull_neg_1 = NEG_1;
+	unsigned u_2g = SZ_2G;
+	unsigned long long ull_2g_two_cast = u_2g;
+	printf("0x%016llx\n", ull_2g);
+	printf("0x%016llx\n", ull_neg_1);
+	printf("0x%016llx\n", ull_2g_two_cast);
+}
