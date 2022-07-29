9b908a33f3d12a972d9d6afb42dbfa3cb1be38a5
Boris Burkov <boris@bur.io>
2022-07-28 17:32:58 -0700

    fiddle with sign extension script

--- c/sign-ext.c
+++ c/sign-ext.c
@@ -8,7 +8,9 @@ int main() {
 	unsigned long long ull_neg_1 = NEG_1;
 	unsigned u_2g = SZ_2G;
 	unsigned long long ull_2g_two_cast = u_2g;
+	unsigned long long ull_neg1_nomacro = -1;
 	printf("0x%016llx\n", ull_2g);
 	printf("0x%016llx\n", ull_neg_1);
+	printf("0x%016llx\n", ull_neg1_nomacro);
 	printf("0x%016llx\n", ull_2g_two_cast);
 }
