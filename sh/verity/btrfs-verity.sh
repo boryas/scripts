da2caef9dff7a0a179bd4fb939bfaae2fcf88bb6
Boris Burkov <boris@bur.io>
2022-08-01 23:08:01 +0000

    boilerplate; verity scripts

---
+++ sh/verity/btrfs-verity.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "$SCRIPT")
+SH_ROOT=$(dirname "$DIR")
+source "$SH_ROOT/boilerplate"
+
+_cleanup() {
+	sudo umount -q "$mnt" || true
+}
+
+MODE_FILE="/proc/sys/fs/verity/mode"
+disable_verity() {
+	echo "disable" | sudo tee "$MODE_FILE"
+}
+audit_verity() {
+	echo "audit" | sudo tee "$MODE_FILE"
+}
+enforce_verity() {
+	echo "enforce" | sudo tee "$MODE_FILE"
+}
+
+if [ $# -lt 4 ]
+then
+	echo "usage: btrfs-verity.sh dev mnt corrupt_off corrupt_sz" >&2
+	exit 1
+fi
+
+dev=$1
+mnt=$2
+corrupt_off=$3
+corrupt_sz=$4
+
+enforce_verity
+
+sudo umount -q "$mnt" || true
+sudo mkfs.btrfs -f "$dev" >/dev/null
+sudo mount "$dev" "$mnt" -o nodatasum
+
+f="$mnt/foo"
+cp "$DIR/exe" "$f"
+sudo xfs_io -c sync "$mnt"
+sudo fsverity enable "$f"
+
+sha256sum "$f"
+
+fiemap_off=$(sudo xfs_io -r -c fiemap "$f" | grep '\[' | cut -d: -f3 | awk -F '.' '{print $1}')
+file_off=$(sudo btrfs-map-logical -l $((512 * fiemap_off)) "$dev" | awk '{print $4}' | head -1)
+corrupt_off=$((file_off + corrupt_off))
+
+sudo umount "$mnt"
+sudo dd if=/dev/zero of="$dev" bs=1 count="$corrupt_sz" seek="$corrupt_off" 2>/dev/null
+sudo mount "$dev" "$mnt" -o nodatasum
+
+set +e
+echo 3 | sudo tee /proc/sys/vm/drop_caches
+"$f" && _fail "ran corrupted $f without error"
+sha256sum "$f" && _fail "sha-d $f without error"
+
+_ok
---
+++ sh/verity/send-recv.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "$SCRIPT")
+SH_ROOT=$(dirname "$DIR")
+source "$SH_ROOT/boilerplate"
+
+BTRFS="/home/vmuser/btrfs-progs/btrfs"
+
+_cleanup() {
+	sudo umount -q "$mnt" || true
+}
+
+_fsv_enable() {
+	local f=$1
+	local tmp=$2
+	local salt="deadbeef"
+	sysctl fs.verity.require_signatures=1
+	openssl req -newkey rsa:4096 -nodes -keyout $tmp/key.pem -x509 -out $tmp/cert.pem
+	openssl x509 -in $tmp/cert.pem -out $tmp/cert.der -outform der
+	keyctl padd asymmetric '' %keyring:.fs-verity < $tmp/cert.der
+	#fsverity sign $f $tmp/file.sig --key=$tmp/key.pem --cert=$tmp/cert.pem --salt=$salt
+	#fsverity enable $f --signature=$tmp/file.sig --salt=$salt || _fail "fsverity enable failed"
+	fsverity sign $f $tmp/file.sig --key=$tmp/key.pem --cert=$tmp/cert.pem
+	fsverity enable $f --signature=$tmp/file.sig || _fail "fsverity enable failed"
+}
+
+if [ $# -lt 2 ]
+then
+	echo "usage: send-recv.sh dev mnt" >&2
+	exit 1
+fi
+
+dev=$1
+mnt=$2
+subv=$mnt/subv
+f="$subv/f"
+tmp=$mnt/tmp
+stream=/tmp/fsv.ss
+
+mkfs.btrfs -f "$dev"
+mount "$dev" "$mnt"
+
+mkdir -p $tmp
+$BTRFS subvol create $subv
+dd if=/dev/zero of=$f bs=4k count=3
+
+_fsv_enable $f $tmp || _fail "fs-verity enable failed"
+chmod a+x $f || _fail "chmod failed"
+setcap cap_net_raw=ep $f || _fail "setcap failed"
+
+$BTRFS property set $subv ro true || _fail "property set failed"
+$BTRFS send $subv -f $stream || _fail "send failed"
+fsverity measure $f > /tmp/measure1 || _fail "measure failed"
+$BTRFS inspect-internal dump-tree $dev
+
+umount "$dev"
+mkfs.btrfs -f "$dev"
+mount "$dev" "$mnt"
+
+$BTRFS receive --dump -f $stream || _fail "recv failed"
+$BTRFS receive $mnt -f $stream || _fail "recv failed"
+echo 3 > /proc/sys/vm/drop_caches || _fail "drop caches failed"
+fsverity measure $f > /tmp/measure2 || _fail "measure failed"
+
+diff /tmp/measure1 /tmp/measure2 || _fail "measurement changed"
+
+_cleanup
+_ok
