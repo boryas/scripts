e4faacd07b805d6cb4af4ccf272964889a4cacd7
Boris Burkov <boris@bur.io>
2023-02-13 19:26:34 +0000

    size class reload

--- sh/size-class/reload.sh
+++ sh/size-class/reload.sh
@@ -12,10 +12,20 @@ dev=$1
 mnt=$2
 f=$mnt/f
 
-_fresh_btrfs_mnt $dev $mnt
+_fresh_btrfs_mnt $dev $mnt -o clear_cache,space_cache=v1
 
-dd if=/dev/zero of=$f bs=1M count=1 # sz class = 2
+_kmsg "write first 16K, 1M, 16M file"
+dd if=/dev/zero of=$f.1 bs=16K count=1 # sz class = 1
+dd if=/dev/zero of=$f.2 bs=1M count=1 # sz class = 2
+dd if=/dev/zero of=$f.3 bs=16M count=1 # sz class = 3
+sync
+cat $(_btrfs_sysfs $dev)/allocation/data/size_classes
 
-_cycle_mount $dev $mnt
+_cycle_mnt $dev $mnt
 
-dmesg | tail -n 25
+_kmsg "write second 16M file"
+dd if=/dev/zero of=$f.4 bs=16M count=1 # sz class = 3
+sync
+cat $(_btrfs_sysfs $dev)/allocation/data/size_classes
+
+#dmesg | tail -n 25
