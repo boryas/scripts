53f0fd25896feb5f3e72e02052016b7c13a514be
Boris Burkov <boris@bur.io>
2023-08-29 14:59:00 -0700

    add chunk write script

---
+++ c/chunk-write.c
@@ -0,0 +1,77 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+#define M 1<<20
+#define G 1<<30
+#define PREALLOC G
+#define CHUNK (100*M)
+
+static char *F = "/mnt/lol/f";
+
+int main(int argc, char **argv) {
+	int ret;
+	int fd;
+	char *buf;
+	struct iovec iov;
+	int iovcnt;
+	unsigned int off;
+
+	fd = open(F, O_WRONLY | O_CREAT);
+	if (fd == -1) {
+		fprintf(stderr, "open failed: %d\n", errno);
+		exit(errno);
+	}
+
+        ret = posix_fallocate(fd, 0, PREALLOC);
+	if (ret) {
+		fprintf(stderr, "fallocate failed: %d\n", ret);
+		ret = errno;
+		goto close;
+	}
+
+	buf = malloc(CHUNK);
+	if (!buf) {
+		fprintf(stderr, "malloc chunk failed\n");
+		ret = ENOMEM;
+		goto close;
+	}
+
+
+	iov.iov_base = buf;
+	for (int i = 0; i < PREALLOC / CHUNK; ++i) {
+		iov.iov_len = CHUNK;
+		off = i * CHUNK;
+		memset(buf, i % 256, CHUNK);
+		while (iov.iov_len) {
+			ret = pwritev(fd, &iov, iovcnt, off);
+			if (ret == -1) {
+				if (errno == -EINTR)
+					continue;
+				fprintf(stderr, "pwritev failed: %d\n", errno);
+				ret = errno;
+				goto free;
+			}
+			iov.iov_len -= ret;
+			off += ret;
+		}
+	}
+
+	ret = fsync(fd);
+	if (ret) {
+		fprintf(stderr, "fsync failed: %d\n", errno);
+		ret = errno;
+		goto free;
+	}
+
+	ret = 0;
+free:
+	free(buf);
+close:
+	close(fd);
+	exit(ret);
+}
