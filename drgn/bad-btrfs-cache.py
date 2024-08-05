22d1addfd7e78512d4ab905bb0ea0689992fe2ee
Boris Burkov <boris@bur.io>
2024-08-05 14:49:24 -0700

    two helpful btree debug drgn scripts

---
+++ drgn/bad-btrfs-cache.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env drgn
+
+import json
+import sys
+
+import drgn
+from drgn import FaultError, NULL, Object, cast, container_of, execscript, offsetof, reinterpret, sizeof, stack_trace
+from drgn.helpers.linux import *
+from drgn.helpers.common import *
+from collections import defaultdict
+
+fs_info = cast("struct btrfs_fs_info *", path_lookup("/").mnt.mnt_sb.s_fs_info)
+btree_inode = fs_info.btree_inode
+mapping = btree_inode.i_mapping
+PG_locked = prog["PG_locked"].value_()
+PG_dirty = prog["PG_dirty"].value_()
+PG_writeback = prog["PG_writeback"].value_()
+PG_private = prog["PG_private"].value_()
+PG_private_2 = prog["PG_private_2"].value_()
+eb_dirty = prog["EXTENT_BUFFER_DIRTY"]
+eb_writeback = prog["EXTENT_BUFFER_WRITEBACK"]
+eb_tree_ref = prog["EXTENT_BUFFER_TREE_REF"]
+PG_PRIVATE = 1 << PG_private | 1 << PG_private_2
+PAGE_SIZE = int(prog["PAGE_SIZE"])
+
+Total = 0
+Fails = defaultdict(int)
+Ok = 0
+EbRefCounts = defaultdict(int)
+Ebs = set()
+
+def bump_fail(fail):
+    Fails[fail] += PAGE_SIZE
+
+def folio_has_private(folio):
+    pflags = folio.flags.value_()
+    return int(bool(pflags & PG_PRIVATE))
+
+def release_eb(eb):
+    ebflags = eb.bflags
+    ebrc = int(eb.refs.counter)
+    EbRefCounts[ebrc] += 1
+    if ebrc == 0:
+        bump_fail("eb-zero-refcount")
+        return False
+    if ebrc > 1:
+        bump_fail("eb-refcount")
+        return False
+    if ebflags & (1 << eb_dirty):
+        bump_fail("eb-dirty")
+        return False
+    if ebflags & (1 << eb_writeback):
+        bump_fail("eb-writeback")
+        return False
+    if ebflags & (1 << eb_tree_ref):
+        bump_fail("eb-tree-ref")
+        return False
+    return True
+
+def release_folio(folio):
+    pflags = folio.flags.value_()
+    if not pflags & (1 << PG_private):
+        bump_fail("no-private")
+        return False
+    eb = cast("struct extent_buffer *", folio.private)
+    return release_eb(eb)
+
+def mapping_evict_folio(mapping, folio):
+    pflags = folio.flags.value_()
+    if pflags & (1 << PG_locked):
+        bump_fail("folio-locked")
+        return False
+    if pflags & (1 << PG_dirty):
+        bump_fail("folio-dirty")
+        return False
+    if pflags & (1 << PG_writeback):
+        bump_fail("folio-writeback")
+        return False
+    if folio._refcount.counter > 1 + folio_has_private(folio) + 1:
+        bump_fail("folio-refcount")
+        return False
+    return release_folio(folio)
+
+flag_hist = defaultdict(int)
+flag_hist_full = defaultdict(int)
+
+def update_flag_hist(folio):
+    flags = decode_page_flags(folio)
+    flag_hist_full[flags] += 1
+    for flag in flags.split("|"):
+        flag_hist[flag] += 1
+
+for index, entry in xa_for_each(mapping.i_pages.address_of_()):
+    folio = cast("struct folio *", entry)
+    try:
+        Total += PAGE_SIZE
+        if (Total >> 20) % 10 == 0:
+            sys.stdout.write(f"\rScanned {Total >> 20}MiB")
+            sys.stdout.flush()
+        update_flag_hist(folio)
+        
+        #if mapping_evict_folio(mapping, folio):
+            #Ok += PAGE_SIZE
+    except drgn.FaultError:
+        bump_fail("drgn-fault-error")
+        continue
+
+Total_GiB = Total >> 30
+print()
+print(f"Total: {Total} Total GiB: {Total_GiB}")
+print(f"Fails: {Fails}")
+json.dump(flag_hist_full, sys.stdout, indent=4)
+print()
+json.dump(flag_hist, sys.stdout, indent=4)
+print()
+exit(0)
+
+Ok_GiB = Ok >> 30
+Fails_GiB = {reason: bs >> 30 for reason, bs in Fails.items()}
+print(f"Total: {Total} Total GiB: {Total_GiB}")
+print(f"Ok: {Ok} Ok GiB: {Ok_GiB}")
+print(f"Fails: {Fails}")
+print(f"Fails GiB: {Fails_GiB}")
+print(f"Eb Ref Counts: {EbRefCounts}")
+if Ok + sum([bs for reason, bs in Fails.items()]) != Total:
+    print("Mismatched amounts!")
---
+++ drgn/untracked-cache2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env drgn
+
+import sys
+
+import drgn
+from drgn import Object
+from drgn.helpers.linux import (
+    for_each_page,
+    inode_path,
+)
+
+def mapping_to_name(mapping):
+    if not m.host:
+        return "no inode"
+    p = inode_path(m.host)
+    if not p:
+        return mapping.a_ops
+    return p
+
+memcg_ssid = prog["memory_cgrp_id"].value_()
+PG_head = prog["PG_head"].value_()
+PG_slab = prog["PG_slab"].value_()
+PG_swapbacked = prog["PG_swapbacked"].value_()
+PAGE_MAPPING_ANON = 0x1
+PAGE_MAPPING_MOVABLE = 0x2
+PAGE_MAPPING_FLAGS = PAGE_MAPPING_ANON | PAGE_MAPPING_MOVABLE
+MEMCG_DATA_OBJCGS = prog["MEMCG_DATA_OBJCGS"].value_()
+MEMCG_DATA_KMEM = prog["MEMCG_DATA_KMEM"].value_()
+__NR_MEMCG_DATA_FLAGS = prog["__NR_MEMCG_DATA_FLAGS"].value_()
+MEMCG_DATA_FLAGS_MASK = __NR_MEMCG_DATA_FLAGS - 1
+
+nr_scanned = 0
+mappings = {}
+try:
+    for page in for_each_page(prog):
+        nr_scanned += 1
+        if nr_scanned % 8192 == 0:
+            sys.stdout.write(f"\rScanned {nr_scanned / 256:.0f}MB")
+            sys.stdout.flush()
+        try:
+            memcg_data = page.memcg_data.value_()
+            if memcg_data & (MEMCG_DATA_OBJCGS | MEMCG_DATA_KMEM):
+                continue
+            memcg = memcg_data & ~MEMCG_DATA_FLAGS_MASK
+            if not memcg:
+                continue
+            memcg_obj = Object(prog, "struct mem_cgroup", address=memcg)
+            if memcg_obj.address_of_() != prog['root_mem_cgroup']:
+                continue
+
+            pflags = page.flags.value_()
+
+            if pflags & (1 << PG_slab):
+                continue
+            if page.compound_head.value_() & 1: # tail page
+                continue
+
+            mapping = page.mapping.value_()
+            if pflags & (1 << PG_swapbacked) and page.private.value_() != 0:
+                continue
+            if mapping & PAGE_MAPPING_ANON:
+                continue
+
+            pgsz = 2 << 20 if pflags & (1 << PG_head) else 4 << 10
+
+            if mapping in mappings:
+                mappings[mapping] += pgsz
+            else:
+                mappings[mapping] = pgsz
+        except drgn.FaultError:
+            pass
+except KeyboardInterrupt:
+    pass
+print("")
+
+print("printing results")
+for mapping in mappings:
+    m = Object(prog, 'struct address_space', address=mapping)
+    print("  %s: %d\n" % (mapping_to_name(m), mappings[mapping]))
